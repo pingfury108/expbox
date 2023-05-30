fdef34dd4cadc188feaab70af2e0932ed9028879
pingfury <pingfury@outlook.com>
2023-05-31 00:23:43 +0800

    fix display text

--- content.js
+++ content.js
@@ -3,7 +3,10 @@ console.log("hello");
 
 items = $(".regulateItem");
 
-$.each( items, function( index, item ){
+$.each(items, function( index, item ){
+    $(".tab:eq(1)",item).css({"display": "-webkit-box", "width": "800px"});
+    new_item = $(".tab:eq(1) > tbody",item); //.wrap("<div class='expbox'></div>");
+
     dd = {};
     dd.to = $(".tab-blank > tbody > tr > td:eq(1)", item).text();
     dd.th = $(".tab:eq(0) > tbody > tr > td:eq(1)", item).text();
@@ -23,4 +26,23 @@ $.each( items, function( index, item ){
     }
 
     console.log(dd);
+
+    fj_text = "";
+
+    for (i in dd.fj){
+        ff = dd.fj[i];
+        text = "入住时间: " + ff.rzTime + "\n\t" +
+            ff.ft + ": " + ff.count + " 间\n";
+
+        fj_text = fj_text + text + "\n\t";
+    }
+
+    ff_div = new_item.after("<div style='float:right;'>" +
+                            "<textarea style='width: 300%;height: 97.5%;'>" +
+                            "酒店名称: " + dd.to + "\n" +
+                            "团号: " + dd.th + "\n" +
+                            "导游信息: " + dd.du + "\n" +
+                            "用房信息: " + "\n\t" +
+                            fj_text + "\n" +
+                            "</textarea>" + "</div>");
 });
