ba2486e6b5eba74d463c136cc3557da0e9011201
pingfury <pingfury@outlook.com>
2023-05-31 23:55:54 +0800

    fix display

--- content.js
+++ content.js
@@ -1,8 +1,5 @@
 
-console.log("hello");
-
 items = $(".regulateItem");
-
 $.each(items, function( index, item ){
     $(".tab:eq(1)",item).css({"display": "-webkit-box", "width": "800px"});
     new_item = $(".tab:eq(1) > tbody",item); //.wrap("<div class='expbox'></div>");
@@ -12,37 +9,37 @@ $.each(items, function( index, item ){
     dd.th = $(".tab:eq(0) > tbody > tr > td:eq(1)", item).text();
     dd.du = $(".tab:eq(0) > tbody > tr > td:eq(3)", item).text();
 
-    dd.fj = [];
+    dd.fj = {};
     n = $(".tab:eq(1) > tbody > tr", item).length;
 
     for (i = 1; i< n-2; i++){
-        ff = {};
         ff_item = $(".tab:eq(1) > tbody > tr:eq(" + i + ")", item);
+        rzTime = $("td:eq(1)", ff_item).text();
+        fj_t = [];
+
+        if (dd.fj[rzTime])
+            fj_t = dd.fj[rzTime];
+
+        ff = {};
         ff.ft = $("td:eq(0)", ff_item).text();
-        ff.rzTime = $("td:eq(1)", ff_item).text();
         ff.count = $("td:eq(5)", ff_item).text();
+        fj_t.push(ff);
 
-        dd.fj.push(ff);
+        dd.fj[rzTime] = fj_t;
     }
-
-    console.log(dd);
 
     fj_text = "";
 
     for (i in dd.fj){
         ff = dd.fj[i];
-        text = "入住时间: " + ff.rzTime + "\n\t" +
-            ff.ft + ": " + ff.count + " 间\n";
+        ft_text = "";
+        for (t in ff){
+            ft_text = `${ft_text}${ff[t].ft}: ${ff[t].count} 间;  `;
+        }
+        text = `\n\t入住时间: ${i}\n\t  ${ft_text}\n`;
 
-        fj_text = fj_text + text + "\n\t";
+        fj_text = fj_text + text;
     }
 
-    ff_div = new_item.after("<div style='float:right;'>" +
-                            "<textarea style='width: 300%;height: 97.5%;'>" +
-                            "酒店名称: " + dd.to + "\n" +
-                            "团号: " + dd.th + "\n" +
-                            "导游信息: " + dd.du + "\n" +
-                            "用房信息: " + "\n\t" +
-                            fj_text + "\n" +
-                            "</textarea>" + "</div>");
+    ff_div = new_item.after(`<div class='expbox' style='float:right;'> <textarea style='width: 300%;height: 97.5%;'>酒店名称: ${dd.to}\n团号: ${dd.th}\n导游信息: ${dd.du}\n用房信息: ${fj_text}</textarea></div>`);
 });
