3970cfe16d94dcbe35bd23392131ed840ae740aa
pingfury <pingfury@outlook.com>
2026-03-06 15:38:27 +0800

    fix: normalize date format and indentation
    - Restore original date format without month conversion
    - Standardize indentation to 2 spaces in summary_hket

--- content.js
+++ content.js
@@ -67,52 +67,51 @@ function summary_hket() {
     var cc = $("#parent-iframe-control > #div1 > #printDiv", iframe.contents());
     var team_info = $("table:eq(2)", cc);
     dd = {};
-    dd.th = $("tbody > tr > td:eq(1)", team_info).text();
-    dd.du = $("tbody > tr > td:eq(3)", team_info).text().replaceAll("\n", "").replaceAll(" ", "");
+  dd.th = $("tbody > tr > td:eq(1)", team_info).text();
+  dd.du = $("tbody > tr > td:eq(3)", team_info).text().replaceAll("\n", "").replaceAll(" ", "");
 
-    var items = $("#tbQuo > tbody > tr:gt(0)", cc);
-    dd.fj = {};
+  var items = $("#tbQuo > tbody > tr:gt(0)", cc);
+  dd.fj = {};
 
-    for (i = 0; i < items.length; i++) {
-        item = items[i];
-        rzTime = $("td:eq(0)", item).text();
+  for (i = 0; i < items.length; i++) {
+    item = items[i];
+    rzTime = $("td:eq(0)", item).text();
 
-        fj_t = []
-        if (dd.fj[rzTime]) {
-            fj_t = dd.fj[rzTime];
-        }
-        ff = {};
-        ff.ft = $("td:eq(1)", item).text().split("|")[1].replaceAll(" ", "");
-        ff.count = $("td:eq(4)", item).text();
-        fj_t.push(ff);
-
-        dd.fj[rzTime] = fj_t;
+    fj_t = []
+    if (dd.fj[rzTime]) {
+      fj_t = dd.fj[rzTime];
     }
+    ff = {};
+    ff.ft = $("td:eq(1)", item).text().split("|")[1].replaceAll(" ", "");
+    ff.count = $("td:eq(4)", item).text();
+    fj_t.push(ff);
 
-    fj_text = [];
+    dd.fj[rzTime] = fj_t;
+  }
 
-    for (i in dd.fj) {
-        ff = dd.fj[i];
-        ft_text = [];
-        for (t in ff) {
-            ft_text.push(`${ff[t].count} ${ff[t].ft}`);
-        }
-        text = `${i.split("-").join("月")}日, 用房: ${ft_text.join(" + ")}`;
+  fj_text = [];
 
-        fj_text.push(text);
+  for (i in dd.fj) {
+    ff = dd.fj[i];
+    ft_text = [];
+    for (t in ff) {
+      ft_text.push(`${ff[t].count} ${ff[t].ft}`);
     }
-    var new_item = $("#parent-iframe-control > #div1", iframe.contents());//.wrap("<div class='expbox'></div>");
-    var offset = 60;
-    w = ($("#parent-iframe-control", iframe.contents()).width() - new_item.width()) / 2 - offset;
-    h = new_item.height() - 4.5;
+    text = `${i}日, 用房: ${ft_text.join(" + ")}`;
 
-    text_1 = `<div class='expbox' style='position: absolute; left: ${w + offset + 50 + new_item.width()}px; margin: 60px auto auto;'>
+    fj_text.push(text);
+  }
+  var new_item = $("#parent-iframe-control > #div1", iframe.contents());//.wrap("<div class='expbox'></div>");
+  var offset = 60;
+  w = ($("#parent-iframe-control", iframe.contents()).width() - new_item.width()) / 2 - offset;
+  h = new_item.height() - 4.5;
+
+  text_1 = `<div class='expbox' style='position: absolute; left: ${w + offset + 50 + new_item.width()}px; margin: 60px auto auto;'>
                             <textarea style='width: ${w}px; height: ${h}px;'>团号: ${dd.th}\n导游信息: ${dd.du}\n原订: \n现麻烦变更为:\n\t${fj_text.join("\n\t")}\n确认变更回复，谢谢</textarea></div>`
-
-    text_2 = `<div class='expbox' style='position: absolute; left: 10px; margin: 60px auto auto;'>
+  text_2 = `<div class='expbox' style='position: absolute; left: 10px; margin: 60px auto auto;'>
 <textarea style='width: ${w}px; height: ${h}px;'>用房信息:\n\t${fj_text.join("\n\t")}\n麻烦收到确认回复,谢谢</textarea></div>`
 
-    remove_summary_hket();
+  remove_summary_hket();
     $("#parent-iframe-control", iframe.contents()).css({ "display": "-webkit-box" });
     ff_div = new_item.after(text_1);
     fb_div = new_item.before(text_2);
